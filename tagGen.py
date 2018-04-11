3f5a3c67bd9ec75a363ad42207c60c065a3118ed
Luke Grehan <luke.grehan17@imperial.ac.uk>
2018-04-11 21:51:57 +0100

    init

---
+++ tagGen.py
@@ -0,0 +1,51 @@
+from contextlib import contextmanager
+
+class Doc:
+    """
+    Generate xml/html documents using context managers
+
+    c.f: https://docs.python.org/3/library/contextlib.html#contextlib.contextmanager
+    """
+
+    def __init__(self):
+        self._result = []
+
+    def text(self, *args, safe=False):
+        txt = []
+        for atom in args:
+            atom = str(atom)
+            if not safe:
+                atom = html_escape(atom)
+            txt.append(atom)
+        self._result.append(''.join(txt))
+
+    @contextmanager
+    def tag(self, name, self_closing=False, *attrs, **kwattrs):
+        endTag = ">" if not self_closing else "/>"
+        self.text("<", name, _mkAttribs(attrs, kwattrs), endTag, safe=True)
+        yield
+        if not self_closing:
+            self.text("</", name, ">", safe=True)
+
+    def line(self, name, cont, *args, **kwargs):
+        with self.tag(name, *args, **kwargs):
+            self.text(cont)
+
+    def render(self):
+        return '\n'.join(self._result)
+
+
+def _mkAttribs(attrs, kwattrs):
+    attrs = list(map(lambda a:' "'+str(a)+'"', attrs))
+    for (k,v) in kwattrs.items():
+        if k == "klass":
+            k = "class"
+        attrs.append(f' {k}="{str(v)}"')
+    return ''.join(attrs)
+
+def html_escape(txt):
+    return str(txt).replace("&", "&amp;")\
+                   .replace("<", "&lt;")\
+                   .replace(">", "&gt;")\
+                   .replace('"', "&quot;")
+
